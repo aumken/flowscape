63c545278012847db03c5658eeb79a0f70822fef
aum kendapadi <aum@unc.edu>
2023-03-19 22:35:39 -0400

    canvas sizing edits

--- sketch.js
+++ sketch.js
@@ -1,26 +1,24 @@
 let particles = [];
-const numberOfParticles = 20000;
+const numberOfParticles = 10000;
 const noiseScale = 0.01;
 
 function setup() {
   // 
-  createCanvas(1500, 1000);
+  createCanvas(750, 250);
   for(let i = 0; i < numberOfParticles; i++) {
     particles.push(createVector(random(width), random(height)));
   }
 }
 
 function draw() {
-  background(220,10);
+  background(200,10);
   for(let i = 0; i < numberOfParticles; i++) {
     let a = particles[i]
     point(a.x, a.y);
     let b = noise(a.x * noiseScale, a.y * noiseScale);
     let c = 2 * PI * b;
-
     a.x -= cos(c);
     a.y -= sin(c);
-
     if (!visible(a)) {
       a.x = random(width);
       a.y = random(height);
